fde7ef72c103f9f7a5ef1c2c2c63272a5e86115f
Vishakhasenthilnathan <56632158+Vishakhasenthilnathan@users.noreply.github.com>
2023-09-29 11:57:43 +0530

    Time: 2 ms (75.84%), Space: 61.3 MB (24.77%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,25 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = prices[0];
+        int minBuy = prices[0];
         int maxProfit = 0;
-        for(var price : prices){
-            if(price < min){
-                min = price;
-            }
-            if(price - min > maxProfit){
-                maxProfit = price - min;
-            }
+        int cost = 0;
+        //My sln
+        //  for(var i=1;i<prices.length;i++){
+        //     if(prices[i]<=min){
+        //         min = prices[i];
+        //     }
+        //     //so price is greater than min, so we can sell. check the max profit
+        //     if(prices[i]-min > max){
+        //         max = prices[i]-min;
+        //     }
+        // }
+
+
+        //DP (diff version of same answer)
+        for(var i=1;i<prices.length;i++){
+            cost = prices[i] - minBuy;
+            maxProfit = Math.max(maxProfit, cost);
+            minBuy = Math.min(minBuy,prices[i]);
         }
         return maxProfit;
     }
