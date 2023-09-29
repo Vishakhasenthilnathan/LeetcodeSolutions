27fbd02d750d5d91f5ad9886898ab76efbb8d796
Vishakhasenthilnathan <56632158+Vishakhasenthilnathan@users.noreply.github.com>
2023-09-29 20:32:59 +0530

    Time: 31 ms (67.98%), Space: 50.7 MB (52.45%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,10 +5,10 @@ class Solution {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         int n =nums.length;
         for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int j = i+1;
             int k = n-1;
-            if(i>0 && nums[i]==nums[i-1]) continue;
-
+            
             while(j<k){
                 if(nums[i]+nums[j]+nums[k]==0){
                     List<Integer> list = Arrays.asList(nums[i],nums[j],nums[k]);
