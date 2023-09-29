198cf25f542d7026e6965f857d8dbcd6162cfb28
Vishakhasenthilnathan <56632158+Vishakhasenthilnathan@users.noreply.github.com>
2023-09-29 21:18:28 +0530

    Time: 15 ms (32.64%), Space: 44.2 MB (32%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,12 +3,7 @@ class Solution {
         HashMap<Character,Integer> magazineMap = new HashMap<>();
         for(var mag : magazine.toCharArray()){
             int val = magazineMap.getOrDefault(mag,0);
-            if(val == 0){
-                magazineMap.put(mag,1);
-            }
-            else{
-                magazineMap.put(mag,val+1);
-            }
+            magazineMap.put(mag,val+1);
         }
 
         for(var ran : ransomNote.toCharArray()){
