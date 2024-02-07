87dd8e2561ad3529c95b1ede9dc819317f8259be
Vishakhasenthilnathan <56632158+Vishakhasenthilnathan@users.noreply.github.com>
2024-02-08 00:07:34 +0530

    Time: 10 ms (32.12%) | Memory: 58.6 MB (25.04%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -5,11 +5,11 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
         for(int val: nums){
             if(val>0){
-                //This takes O(n)
+                //This takes O(1)
                 set.add(val);
             }
         }
-
+        //we can find in o(n) loop i.e o(n)+o(1) = o(n)
         for(int i=1;i<=set.size()+1;i++){
             if(!set.contains(i)){
                 return i;
