97d9f77ec13cac3d632453a138ab13528fb58d1a
Vishakhasenthilnathan <56632158+Vishakhasenthilnathan@users.noreply.github.com>
2024-02-11 21:41:51 +0530

    Time: 6 ms (95.07%) | Memory: 47.6 MB (61.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,8 +3,12 @@ class Solution {
         HashMap<String, List<String>> map = new HashMap<>();
         for(String str: strs){
             char[] keyArr = str.toCharArray(); 
-            Arrays.sort(keyArr);
-            String key = String.valueOf(keyArr);
+            // Arrays.sort(keyArr);
+            char[] sortArr = new char[26];
+            for(int i=0;i<str.length();i++){
+                sortArr[str.charAt(i)-97]++;
+            }
+            String key = String.valueOf(sortArr);
             
             map.putIfAbsent(key,new ArrayList<String>());
             map.get(key).add(str);
