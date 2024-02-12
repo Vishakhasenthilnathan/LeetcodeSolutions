11cd4eda2b83d2a6fb087d172bf5a285c08bead4
Vishakhasenthilnathan <56632158+Vishakhasenthilnathan@users.noreply.github.com>
2024-02-13 00:38:08 +0530

    Time: 1 ms (99.73%) | Memory: 57.5 MB (16.29%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,16 +1,20 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+    
+        //KADANE ALGORITHM
         int currentSum = 0;
-		int maxSum = Integer.MIN_VALUE;
+        int maxSum = Integer.MIN_VALUE;
 
-		for(int i=0;i<nums.length;i++){
-			currentSum += nums[i];
-			if(currentSum > maxSum)
-				maxSum = currentSum;
+        for(int i=0;i<nums.length;i++){
+            currentSum += nums[i];
+            if(currentSum > maxSum)
+                maxSum = currentSum;
 
-			if(currentSum < 0)
-				currentSum =0;
-		}
-		return maxSum;
+            //Very important - done to avoid negatives in sum to obtain the maximum possible sum of subarray
+            if(currentSum < 0)
+                currentSum = 0;
+        }
+
+        return maxSum;
     }
 }
